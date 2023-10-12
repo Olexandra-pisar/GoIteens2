966b70e18b9d4fdd282c9ffb4f27622b94f5d4b7
Acer <a.pisar099@gmail.com>
2023-10-12 20:10:02 +0300

    add main general flask file

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "This is a main pages/activate"
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
